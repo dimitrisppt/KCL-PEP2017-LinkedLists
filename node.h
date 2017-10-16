970e3ed32ca49804f398d094c1e3c889add12f05
Dimitris Papatheodoulou <dimitris.papatheodoulou@kcl.ac.uk>
2017-10-16 10:43:23 +0100

    Implemented node.h

--- node.h
+++ node.h
@@ -8,23 +8,35 @@ using std::endl;
 
 // TODO your code for the Node class goes here:
 // (Note the 'NodeIterator' class will report lots of errors until Node exists
+template<typename T>
+class Node {
 
+  public:
+    T data;
+    Node<T>* next;
+    Node<T>* previous;
 
+  public:
+    Node(T dataIn)
+        : data(dataIn) {
+          next = nullptr;
+          previous = nullptr;
+    }
 
+};
 
 
 template<typename T>
 class NodeIterator {
-  
+
 private:
-    
+
     Node<T>* current;
-    
+
 public:
-    
 
     NodeIterator(Node<T>* currentIn)
-        : current(currentIn) {        
+        : current(currentIn) {
     }
 
     T & operator*() {
@@ -32,8 +44,19 @@ public:
     }
 
     // TODO: complete the code for NodeIterator here
-        
-    
+    NodeIterator<T>* operator++() {
+      current = current->next;
+      return this;
+    }
+
+    bool operator==(const NodeIterator<T>& itr) {
+      return current==itr.current;
+    }
+
+    bool operator!=(const NodeIterator<T>& itr) {
+      return current!=itr.current;
+    }
+
 };
 
 // do not edit below this line
